76f78f74eab08c19395c22bb6b748b1de5582ed6
Anna Fomina <ann.fomi77@gmail.com>
2025-04-11 15:42:14 +0300

    add drag and drop functionality

--- candy.js
+++ candy.js
@@ -8,6 +8,11 @@ var rows = 9;
 var columns = 9;
 var score = 0;
 
+// The candy you dragging
+var currTile;
+// The candy you dropping onto
+var otherTile;
+
 window.onload = function() {
     startGame();
 }
@@ -28,9 +33,48 @@ function startGame() {
             // Generate a src for img
             tile.src = "./images/" + randomCandy() + ".png";
 
+            // Drag functionality
+            tile.addEventListener("dragstart", dragStart); // Click on a candy, initialize drag process
+            tile.addEventListener("dragover", dragOver); // Mouse btn down, start moving mouse to drag the candy
+            tile.addEventListener("dragenter", dragEnter); // Dragging initial candy on top of another candy
+            tile.addEventListener("dragleave", dragLeave); // Leave candy over another candy
+            tile.addEventListener("drop", dragDrop); // Dropping a candy over another candy, mouse btn up
+            tile.addEventListener("dragend", dragEnd); // After drag process completed, we swap candies
+
             document.getElementById("board").append(tile);
             row.push(tile);
         }
         board.push(row);
     } 
+
+}
+
+function dragStart() {
+    // This refers to tile that was clicked on for dragging
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    // this refers to the target tile that was dropped on
+    otherTile = this;
+}
+
+function dragEnd() {
+    // Swap the image sorces
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
 }
